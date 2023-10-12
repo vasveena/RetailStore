ed27b1ac528eee9133355b949fe595a108b237b1
Veena Vasudevan <vasveena@amazon.com>
2023-10-12 10:02:48 -0400

    rds config

---
+++ .platform/hooks/postdeploy/02_createsuperuser.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source "/var/app/venv/*/bin/activate" && {
+    
+    if [[ $EB_IS_COMMAND_LEADER == "true" ]];
+    then 
+        echo "from accounts.models import Account; Account.objects.create_superuser('Reinvent', 'Admin', 'admin@example.com', 'admin', 'Test123$')" | python manage.py shell
+    else 
+        echo "this instance is NOT the leader";
+    fi
+    
+}
