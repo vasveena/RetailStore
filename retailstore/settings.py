f2b92103822b2c3e9113c6c9b42e33495156f0f3
Veena Vasudevan <vasveena@amazon.com>
2023-10-11 23:01:55 -0400

    cart

--- retailstore/settings.py
+++ retailstore/settings.py
@@ -134,3 +134,16 @@ STATICFILES_DIRS = [
 # media files 
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'xxxx@gmail.com'
+EMAIL_HOST_PASSWORD = 'yyyy'
+EMAIL_USE_TLS = True
