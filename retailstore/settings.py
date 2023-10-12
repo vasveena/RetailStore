72d91f4b3be7228601a7d8916dce5c2b96c2e4df
Veena Vasudevan <vasveena@amazon.com>
2023-10-12 09:39:23 -0400

    rds config

--- retailstore/settings.py
+++ retailstore/settings.py
@@ -87,7 +87,7 @@ AUTH_USER_MODEL = 'accounts.Account'
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
+            'ENGINE': 'django.db.backends.mysql',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
