821cc75ca558d72dd17f535aa2f76a070e014be8
Veena Vasudevan <vasveena@amazon.com>
2023-11-07 13:43:41 -0500

    cf

---
+++ retailstore/storage_backends.py
@@ -0,0 +1,26 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+from decouple import config
+import os
+
+class StaticStorage(S3Boto3Storage):
+
+    def __init__(self, *args, **kwargs):
+        kwargs['custom_domain'] = config('AWS_CLOUDFRONT_DOMAIN')  
+        kwargs['signature_version'] = "s3v4"
+        kwargs['region_name'] = os.environ.get("AWS_DEFAULT_REGION", None)
+        kwargs['default_acl'] = "public-read"
+        kwargs['location'] = "static"
+        
+        super(StaticStorage, self).__init__(*args, **kwargs)
+
+    
+class PublicMediaStorage(S3Boto3Storage):
+
+    location = 'media'
+    file_overwrite = False
+
+    def __init__(self, *args, **kwargs):
+        kwargs['custom_domain'] = config('AWS_CLOUDFRONT_DOMAIN')    
+        kwargs['signature_version'] = "s3v4"
+    
+        super(PublicMediaStorage, self).__init__(*args, **kwargs)
